b79920c5134e499a18ddddda85c9bc0c211d57c0
kamalpratap022 <kamalpratao022@gmail.com>
2024-06-18 11:56:09 +0530

    update runner file

--- runner.py
+++ runner.py
@@ -1,11 +1,17 @@
 import os
-import subprocess
+from datetime import datetime
 import time
+
 
 current_work_dir = os.getcwd()
 # Install dependency
 os.system('python -m pip install --upgrade pip')
 os.system("python -m pip install -r " + os.path.join(current_work_dir, "requirements.txt"))
 
-#time.sleep(10)
-#subprocess.run(["pytest --headed --template=html1/index.html --report=report.html"])
+#os.system("pytest --headed --template=html1/index.html --report=report.html")
+
+os.system("pytest --headed --template=html1/index.html --report=reports/" + datetime.now().strftime("%d-%m-%Y-%H-%M-%S") + ".html")
+
+time.sleep(5)
+# UnInstall dependency
+os.system("python -m pip uninstall -r " + os.path.join(current_work_dir, "requirements.txt") + " -y")
